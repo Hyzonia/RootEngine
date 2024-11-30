b83c06bee53c835a57b3f939653f6f686401be7b
SpigotRCE <spigotrce@gmail.com>
2024-11-30 22:45:24 +0530

    feat: plugin booting

--- src/main/java/io/github/spigotrce/paper/PaperEngine.java
+++ src/main/java/io/github/spigotrce/paper/PaperEngine.java
@@ -1,4 +1,32 @@
 package io.github.spigotrce.paper;
 
-public class PaperEngine {
+import org.bukkit.Server;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.nio.file.Path;
+import java.util.logging.Logger;
+
+public class PaperEngine extends JavaPlugin {
+    public static PaperEngine INSTANCE;
+    public static Logger LOGGER;
+    public static Path DATA_DIRECTORY;
+    public static Server SERVER;
+
+    public PaperEngine() {
+        INSTANCE = this;
+    }
+
+    @Override
+    public void onEnable() {
+        LOGGER = getLogger();
+        DATA_DIRECTORY = getDataFolder().toPath();
+        SERVER = getServer();
+
+        LOGGER.info("Starting PaperEngine");
+    }
+
+    @Override
+    public void onDisable() {
+        LOGGER.info("Stopping PaperEngine");
+    }
 }
--- src/main/java/io/github/spigotrce/velocity/VelocityEngine.java
+++ src/main/java/io/github/spigotrce/velocity/VelocityEngine.java
@@ -1,4 +1,37 @@
 package io.github.spigotrce.velocity;
 
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
+import io.github.spigotrce.common.messaging.PacketFactory;
+import org.slf4j.Logger;
+
+import java.nio.file.Path;
+
 public class VelocityEngine {
+    public static VelocityEngine INSTANCE;
+
+    public static PacketFactory PACKET_FACTORY;
+    @Inject
+    public static Logger LOGGER;
+    @Inject
+    @DataDirectory
+    public static Path DATA_DIRECTORY;
+    @Inject
+    public static ProxyServer PROXY_SERVER;
+
+    @Inject
+    public VelocityEngine(Logger logger, @DataDirectory Path dataDirectory, ProxyServer proxyServer) {
+        INSTANCE = this;
+        LOGGER = logger;
+        DATA_DIRECTORY = dataDirectory;
+        PROXY_SERVER = proxyServer;
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        LOGGER.info("Starting VelocityEngine");
+    }
 }
