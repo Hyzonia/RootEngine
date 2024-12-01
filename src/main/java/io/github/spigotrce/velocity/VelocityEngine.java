dda01a72ee085a7ef46f8c2f83abc3d6cf4d8db3
SpigotRCE <spigotrce@gmail.com>
2024-12-01 07:55:58 +0530

    feat: PacketFactory proxy side

--- src/main/java/io/github/spigotrce/velocity/VelocityEngine.java
+++ src/main/java/io/github/spigotrce/velocity/VelocityEngine.java
@@ -1,20 +1,31 @@
 package io.github.spigotrce.velocity;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.PluginMessageEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.ServerConnection;
 import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
 import io.github.spigotrce.common.messaging.MessagingConstants;
 import io.github.spigotrce.common.messaging.PacketFactory;
+import io.github.spigotrce.common.messaging.impl.CommandForwardPacket;
+import io.github.spigotrce.common.messaging.impl.HandshakePacket;
+import io.github.spigotrce.common.messaging.impl.HandshakeResponsePacket;
+import io.github.spigotrce.common.messaging.impl.NickUpdatePacket;
 import io.github.spigotrce.velocity.database.PlayerDatabase;
 import io.github.spigotrce.velocity.listener.impl.PlayerConnectionListener;
+import io.github.spigotrce.velocity.misc.BackendServer;
 import io.github.spigotrce.velocity.misc.NickManager;
 import org.slf4j.Logger;
 
 import java.nio.file.Path;
+import java.util.Map;
 
 @Plugin(id = "rootengine",
         name = "RootEngine",
@@ -34,6 +45,8 @@ public class VelocityEngine {
     public static PlayerDatabase PLAYER_DATABASE;
 
     public static NickManager NICK_MANAGER;
+
+    public static Map<RegisteredServer, BackendServer> BACKEND_SERVERS;
 
     @Inject
     public static Logger LOGGER;
@@ -58,6 +71,38 @@ public class VelocityEngine {
     public void onProxyInitialization(ProxyInitializeEvent event) {
         LOGGER.info("Starting VelocityEngine");
         registerListeners();
+        initializePacketFactory();
+    }
+
+    private void initializePacketFactory() {
+        PACKET_FACTORY = new PacketFactory(sender -> ((ServerConnection) sender.connection).
+                sendPluginMessage(MinecraftChannelIdentifier.from(MessagingConstants.ENGINE_CHANNEL.channelName), sender.data));
+
+        PACKET_FACTORY.registerPacket("handshake", HandshakePacket::new, handshakePacket -> {
+            // not s -> p
+        });
+
+        PACKET_FACTORY.registerPacket("handshake_response", HandshakeResponsePacket::new, handshakeResponsePacket -> {
+            BACKEND_SERVERS.get(PROXY_SERVER.getServer(handshakeResponsePacket.getServerName()).get()).protocolVersion = handshakeResponsePacket.getServerPVN();
+            LOGGER.debug("Backend server PVN: {}", BACKEND_SERVERS.get(PROXY_SERVER.getServer(handshakeResponsePacket.getServerName()).get()).protocolVersion);
+        });
+
+        PACKET_FACTORY.registerPacket("nick_update", NickUpdatePacket::new, nickUpdatePacket -> {
+            // not s -> p
+        });
+
+        PACKET_FACTORY.registerPacket("command_forward", CommandForwardPacket::new, commandForwardPacket -> {
+            PROXY_SERVER.getCommandManager().executeImmediatelyAsync(PROXY_SERVER.getPlayer(commandForwardPacket.getPlayerUUID()).get(), commandForwardPacket.getCommand());
+        });
+
+        PROXY_SERVER.getChannelRegistrar().register(MinecraftChannelIdentifier.from(MessagingConstants.ENGINE_CHANNEL.channelName));
+        PROXY_SERVER.getEventManager().register(this, PluginMessageEvent.class, PostOrder.NORMAL, event -> {
+            if (!event.getIdentifier().equals(MinecraftChannelIdentifier.from(MessagingConstants.ENGINE_CHANNEL.channelName))) return;
+            event.setResult(PluginMessageEvent.ForwardResult.handled());
+            if (event.getSource() instanceof Player) return;
+
+            PACKET_FACTORY.decodeAndApply(event.getData());
+        });
     }
 
     private void registerListeners() {
